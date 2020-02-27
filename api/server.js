2ea03ce6ddf4b1ea457d44ed09430721a8ae46f4
Hiter Harris <hiterharris@comcast.net>
2020-02-27 12:42:34 -0500

    project setup

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const cohortsRouter = require('../cohorts/router');
+const studentsRouter = require('../students/router');
+
+const server = express();
+
+// Middleware
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+// Routes
+server.use('/api/cohorts', cohortsRouter);
+
+server.use('/api/students', studentsRouter);
+
+server.get('/', (req, res) => {
+    res.json('Server up and running');
+});
+
+module.exports = server;
